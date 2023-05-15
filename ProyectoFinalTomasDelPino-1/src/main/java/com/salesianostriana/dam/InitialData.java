19386884393125f805d60e20fbec14c6ed0fd509
Tomás del Pino Coffey <tomasdelpino28@gmail.com>
2023-05-15 12:12:14 +0200

    minor fixes

--- ProyectoFinalTomasDelPino-1/src/main/java/com/salesianostriana/dam/InitialData.java
+++ ProyectoFinalTomasDelPino-1/src/main/java/com/salesianostriana/dam/InitialData.java
@@ -1,42 +1,42 @@
-package com.salesianostriana.dam;
-
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import com.salesianostriana.dam.models.Usuario;
-import com.salesianostriana.dam.repositories.UsuarioRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@Component
-@RequiredArgsConstructor
-public class InitialData {
-	
-	private final UsuarioRepository repo;
-	private final PasswordEncoder passwordEncoder;
-	
-	@PostConstruct
-	public void init() {
-		
-		Usuario usuario = Usuario.builder()
-				.isAdmin(false)
-				.username("user")
-				//.password("1234")
-				.password(passwordEncoder.encode("1234"))
-				.build();
-		
-		Usuario admin = Usuario.builder()
-				.isAdmin(true)
-				.username("admin")
-				.password(passwordEncoder.encode("admin"))
-				.build();
-		
-		repo.saveAll(List.of(usuario, admin));
-		
-	}
-
-}
+//package com.salesianostriana.dam;
+//
+//import java.util.List;
+//
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import com.salesianostriana.dam.models.Usuario;
+//import com.salesianostriana.dam.repositories.UsuarioRepository;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class InitialData implements ApplicationRunner{
+//	
+//	private final UsuarioRepository repo;
+//	private final PasswordEncoder passwordEncoder;
+//
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//		Usuario usuario = Usuario.builder()
+//				.id(1)
+//				.isAdmin(false)
+//				.username("user")
+//				//.password("1234")
+//				.password(passwordEncoder.encode("1234"))
+//				.build();
+//		
+//		Usuario admin = Usuario.builder()
+//				.isAdmin(true)
+//				.username("admin")
+//				.password("{bcrypt}$2a$12$x3cnYxcqZYrjk9n86jccXu9MEICIFGlO6RiLa/stuzvhytfzyNOki")
+//				.build();
+//		
+//		repo.saveAll(List.of(usuario, admin));
+//		
+//	}
+//
+//}
